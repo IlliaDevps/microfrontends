77440be86550f4632fa1012e0bcf2ef2f62836f4
IlliaDevps <ilianetworker@gmail.com>
2023-08-30 00:58:22 +0200

    adding communication between apps and adding auth

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch,Route, Router} from 'react-router-dom';
 import { StylesProvider, createGenerateClassName, } from '@material-ui/core/styles';
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+/*import Landing from './components/Landing';
+import Pricing from './components/Pricing';*/
+
 //StylesProvider is a higher-order component that allows you to pass styles to your components.
 //Used to customize all the css and js generation styling of your components.
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'ma' 
+    productionPrefix: 'au' 
 });
 //This is the prefix that will be applied to the generated class names. 
 //This is used to generate unique class names for your components.
@@ -19,8 +20,7 @@ export default ({ history }) => {
         <StylesProvider generateClassName={generateClassName}>
             <Router history={history} >
                 <Switch>
-                    <Route exact path="/pricing" component = {Pricing} />
-                    <Route exact path="/" component = {Landing} />
+
                 </Switch>
             </Router>
         </StylesProvider>
--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -28,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 };  
 //If we are in development and in isolation, call mount immediately
 if(node_env === 'development'){
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
     if (devRoot) {  
         mount(devRoot, { defaultHistory: createBrowserHistory() });//When call the mount function when are in insolation we are going to  create a BrowserHistory instance and give it as a property called default history
     }
